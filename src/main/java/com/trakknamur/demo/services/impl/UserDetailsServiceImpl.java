923560923ec6d4cff691886744f52fab6edb617b
Ceuleers Grégory <gregory.ceuleers@bstorm.be>
2020-09-25 10:01:09 +0200

    Add implement BaseService (WIP)

--- src/main/java/com/trakknamur/demo/services/impl/UserDetailsServiceImpl.java
+++ src/main/java/com/trakknamur/demo/services/impl/UserDetailsServiceImpl.java
@@ -1,15 +1,20 @@
 package com.trakknamur.demo.services.impl;
 
 
+import com.trakknamur.demo.models.dtos.UserDTO;
 import com.trakknamur.demo.models.entities.User;
+import com.trakknamur.demo.models.forms.UserForm;
 import com.trakknamur.demo.repositories.UserRepository;
+import com.trakknamur.demo.services.BaseService;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
-public class UserDetailsServiceImpl implements UserDetailsService {
+public class UserDetailsServiceImpl implements UserDetailsService, BaseService<UserDTO, UserForm, Long> {
 
     private final UserRepository userRepository;
 
@@ -27,5 +32,30 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     public String insert(User u) {
         User user = this.userRepository.save(u);
         return user.getUsername();
+    }
+
+    @Override
+    public List<UserDTO> getAll() {
+        return null;
+    }
+
+    @Override
+    public UserDTO getOne(Long id) {
+        return null;
+    }
+
+    @Override
+    public void insert(UserForm form) {
+
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+
+    @Override
+    public UserDTO update(UserForm form, Long id) {
+        return null;
     }
 }
