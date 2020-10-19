d602cc337c31e6935382e475f79874e2037b0dea
Ceuleers Grégory <gregory.ceuleers@bstorm.be>
2020-10-19 21:09:37 +0200

    Add new authentication filter Add JWT dependency Add SecurityConstants

---
+++ src/main/java/com/trakknamur/demo/configs/JwtAuthenticationFilter.java
@@ -0,0 +1,61 @@
+package com.trakknamur.demo.configs;
+
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.trakknamur.demo.models.entities.User;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+
+import static com.trakknamur.demo.configs.SecurityConstants.*;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+
+        try  {
+            User creds = new ObjectMapper().readValue(request.getInputStream(), User.class);
+
+            return authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            creds.getUsername(),
+                            creds.getPassword(),
+                            creds.getAuthorities()
+                    )
+            );
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        //        return super.attemptAuthentication(request, response);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+
+        String token = JWT.create()
+                .withSubject(((User)authResult.getPrincipal()).getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .sign(Algorithm.HMAC512(SECRET.getBytes()));
+
+        response.addHeader(HEADER_STRING, TOKEN_PREFIX + token);
+    }
+}
---
+++ src/main/java/com/trakknamur/demo/configs/SecurityConstants.java
@@ -0,0 +1,11 @@
+package com.trakknamur.demo.configs;
+
+public class SecurityConstants {
+
+    public static final String SECRET = "MacleSecretePourGenererJWTs";
+    public static final long EXPIRATION_TIME = 864000000; // 10 days
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+    public static final String SIGN_UP_URL = "/users/sign-up";
+
+}
